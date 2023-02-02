d12503724506a3a645a202ef0d124c37a67f0c18
giufeli <giuliofelicetti99@gmail.com>
2023-02-02 14:35:49 +0100

    Firts commit

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Componente Vue</h1>
+</template>
+
+<script>
+import { assertExpressionStatement, throwStatement } from '@babel/types';
+import { response } from 'express';
+
+    export default{
+        name: 'App',
+        data() {
+            return {
+                arrPosts: [],
+            };
+        },
+
+        created() {
+            axios.get('/api/post')
+                then(response => this.arrPosts = responde.data.results)
+        }
+    }
+</script>
+
+
---
+++ resources/js/front.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+import Vue from 'vue';
+import App from './App.vue';
+
+
+new Vue({
+    el: '#root',
+    render: h => h(App)
+})
