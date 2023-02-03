de66de6aa628f77ab8de77d0f43b5a09bad3ca41
giufeli <giuliofelicetti99@gmail.com>
2023-02-03 10:02:36 +0100

    Add Vue list

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,24 @@
 <template>
-    <h1>Componente Vue</h1>
+    <div class="container">
+        <h1>Componente Vue</h1>
+
+        <div class="row g-3">
+                <div v-for="post in arrPosts" :key="post.id" class="col-sm-6 col-md-4">
+                    <div class="card h-100">
+                        <img :src="post.image" class="card-img-top" :alt="post.title">
+                        <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">{{ post.title }}</h5>
+                        <p class="card-text flex-grow-1">{{ post.excerpt }}</p>
+                        <a :href="'admin/posts/' + post.id" class="btn btn-primary">Leggi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-import { assertExpressionStatement, throwStatement } from '@babel/types';
-import { response } from 'express';
 
     export default{
         name: 'App',
@@ -15,10 +29,14 @@ import { response } from 'express';
         },
 
         created() {
-            axios.get('/api/post')
-                then(response => this.arrPosts = responde.data.results)
+            axios.get('/api/posts')
+            .then(response => this.arrPosts = response.data.results);
         }
     }
 </script>
+
+<style lang="scss">
+    @import '~bootstrap/scss/bootstrap';
+</style>
 
 
